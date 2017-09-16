86c5c39fd3a6dcaa399756e6a456f20c9e53f336
Justin Meyer <justinbmeyer@gmail.com>
2017-09-15 22:04:14 -0500

    tabs ftw

--- queue-test.js
+++ queue-test.js
@@ -3,15 +3,17 @@ var queue = require("queue");
 
 QUnit.module('queue');
 
-QUnit.test('basics', function(){
+QUnit.test('basics', function() {
 
 	function queueHandlers(handlersByQueue, context, args) {
 		queue.batch.start();
-		["notify","derive","mutate"].forEach(function(queueName){
-			var name = queueName.toUpperCase()+"_QUEUE";
+		["notify", "derive", "mutate"].forEach(function(queueName) {
+			var name = queueName.toUpperCase() + "_QUEUE";
 			var QUEUE = queue[name];
-			(handlersByQueue[queueName] || []).forEach(function(handler){
-				QUEUE.enqueue(handler, context,  args,{log: [handler.name + " by " +context.name]})
+			(handlersByQueue[queueName] || []).forEach(function(handler) {
+				QUEUE.enqueue(handler, context, args, {
+					log: [handler.name + " by " + context.name]
+				})
 			})
 		})
 		queue.batch.stop();
@@ -19,93 +21,99 @@ QUnit.test('basics', function(){
 
 	var gc1 = {
 		name: "gc1",
-	    notifyHandlers: [
-	        function derivedChild_queueUpdate(){
-	            derivedChild.queueUpdate();
-	        }
-	    ],
-	    mutateHandlers: [function gc1_eventHandler_writableChild_dispatch(a,b){
-	        writableChild.dispatch();
-	    }],
-	    dispatch: function(){
+		notifyHandlers: [
+			function derivedChild_queueUpdate() {
+				derivedChild.queueUpdate();
+			}
+		],
+		mutateHandlers: [function gc1_eventHandler_writableChild_dispatch(a, b) {
+			writableChild.dispatch();
+		}],
+		dispatch: function() {
 			queueHandlers({
 				notify: this.notifyHandlers,
 				mutate: this.mutateHandlers
 			}, this, []);
-	    }
+		}
 	};
 
 	var gc2 = {
 		name: "gc2",
-	    notifyHandlers: [
-	        function deriveChild_queueUpdate(){
-	            // derivedChild.update
-	        }
-	    ],
-	    mutateHandlers: [],
-	    dispatch: function(){
+		notifyHandlers: [
+			function deriveChild_queueUpdate() {
+				// derivedChild.update
+			}
+		],
+		mutateHandlers: [],
+		dispatch: function() {
 			queueHandlers({
 				notify: this.notifyHandlers,
 				mutate: this.mutateHandlers
 			}, this, []);
-	    }
+		}
 	}
 
 	var derivedChild = {
 		name: "derivedChild",
-	    queueUpdate: function(){
-	        queue.DERIVE_QUEUE.enqueue(this.update, this, [], {priority: 1, log: ["update on "+this.name]})
-	    },
-	    update: function(){
-	        // check value
-	        // value changed
+		queueUpdate: function() {
+			queue.DERIVE_QUEUE.enqueue(this.update, this, [], {
+				priority: 1,
+				log: ["update on " + this.name]
+			})
+		},
+		update: function() {
+			// check value
+			// value changed
 			queueHandlers({
 				notify: this.notifyHandlers,
 				mutate: this.mutateHandlers
 			}, this, []);
-	    },
+		},
 		notifyHandlers: [
-			function root_queueUpdate(){
+			function root_queueUpdate() {
 				root.queueUpdate();
 			}
 		]
 	}
 	derivedChild.update = derivedChild.update.bind(derivedChild);
 
-	var writableChild =  {
+	var writableChild = {
 		name: "writableChild",
-	    dispatch: function(){
-	        // check value
-	        // value changed
+		dispatch: function() {
+			// check value
+			// value changed
 			queueHandlers({
 				notify: this.notifyHandlers,
 				mutate: this.mutateHandlers
 			}, this, []);
-	    },
+		},
 		notifyHandlers: [
-			function root_queueUpdate(){
+			function root_queueUpdate() {
 				root.queueUpdate();
 			}
 		],
 		mutateHandlers: [
-			function eventHandler(){}
+			function eventHandler() {}
 		]
 	}
 
 	var root = {
 		name: "root",
-		queueUpdate: function(){
-			queue.DERIVE_QUEUE.enqueue(this.update, this, [], {priority: 1, log: ["update on "+this.name]})
+		queueUpdate: function() {
+			queue.DERIVE_QUEUE.enqueue(this.update, this, [], {
+				priority: 1,
+				log: ["update on " + this.name]
+			})
 		},
-		update: function(){
-	        // check value
-	        // value changed
+		update: function() {
+			// check value
+			// value changed
 			queueHandlers({
 				notify: this.notifyHandlers,
 				mutate: this.mutateHandlers
 			}, this, []);
-	    },
-		mutateHandlers: [function eventHandler(){}]
+		},
+		mutateHandlers: [function eventHandler() {}]
 	};
 	root.update = root.update.bind(root);
 
